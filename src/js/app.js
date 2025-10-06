9ed6f59387a50eff6b2f470a0fe5a8fd1c897d9a
Manuel Omar Acosta Dominguez <macosta.ia88@gmail.com>
2025-10-06 03:06:33 +0000

    Conditional.Manu

--- src/js/app.js
+++ src/js/app.js
@@ -23,43 +23,54 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
+  // HTML de forma dinámica
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${
+            variables.includeCover
+              ? `<div class="cover" style="background-image: url('${variables.background}'); background-size: cover; background-position: center;"></div>`
+              : "<div class='cover'></div>"
+          }
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Nombre"} ${variables.lastName ||
+    "Apellido"}</h1>
+          <h2>${variables.role || "Tu Rol"}</h2>
+          <h3>${variables.city || "Ciudad"}, ${variables.country || "País"}</h3>
+          <ul class="${variables.socialMediaPosition || "position-right"}">
+            ${
+              variables.twitter
+                ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.github
+                ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.linkedin
+                ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.instagram
+                ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+                : ""
+            }
           </ul>
         </div>
     `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
+// Lógica para hacer el widget dinámico y mostrar iconos correctamente
 window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should show the cover image
+  // Estado inicial
+  let variables = {
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
+    socialMediaPosition: "position-right",
     twitter: null,
     github: null,
     linkedin: null,
@@ -70,22 +81,21 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+  // Render inicial
+  render(variables);
+
+  // Escuchar cambios en todos los .picker
+  document.querySelectorAll(".picker").forEach(el => {
+    el.addEventListener("change", function(e) {
+      const key = el.getAttribute("for");
+      let value = el.value;
+      // Convertir a boolean si es includeCover
+      if (key === "includeCover") value = value === "true";
+      // Si es vacío, poner null
+      if (value === "") value = null;
+      variables[key] = value;
+      render(variables);
     });
   });
 };
